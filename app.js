40d03c33a73e2186b5a4d1fff0ebe2bc8241d76b
Sneha-p1 <snehaparambadan@gmail.com>
2024-06-25 11:48:22 +0530

    Employee

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.urlencoded({ extended: true }));
 
-const employeeDetails = [];
+var employeeDetails = [];
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
@@ -19,12 +19,20 @@ app.get('/add', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'add.html'))
 })
 
-app.get('/update', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'update.html'))
-})
 
 app.get('/Details', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'EmpDetails.html'))
+})
+
+app.get('/update',(req,res)=>
+{
+    res.sendFile(path.join(__dirname,'public','update.html'))
+})
+
+
+app.get('/search',(req,res)=>
+{
+    res.sendFile(path.join(__dirname,'public','search.html'))
 })
 
 
@@ -43,9 +51,75 @@ app.post('/add',(req,res) =>{
 })
 app.get('/view/data',(req,res) =>{
     res.json(employeeDetails);
-    console.log("ghsd")
 })
 
+app.get('/update/:id',(req,res)=>
+{
+ const emid=req.params.id;
+ const details=employeeDetails.find(emp=>emp.empID==parseInt(emid));
+
+ if(!details){
+    return res.status(404).json({error:'task not found'});
+ }
+
+    console.log("updation id:")
+    console.log(emid);
+    res.sendFile(path.join(__dirname,'public','update.html'))
+    
+})
+
+app.post('/update/data/:id',(req,res)=>
+{
+    let {empID,empName,empRole}=req.body;
+    empID=parseInt(empID)
+    console.log(empID);
+    const update={empID,empName,empRole}
+    const reqid=req.params.id;
+    let details = employeeDetails.findIndex(emp => emp.empID === parseInt(reqid));
+    // 
+    
+    if (details !== -1) {
+        employeeDetails[details] = { ...employeeDetails[details], ...update };
+    }
+    res.redirect('/Details')
+
+})
+
+
+app.get('/search/:id',(req,res)=>
+{
+    const id=req.params.id;
+    const details=employeeDetails.find(emp=>emp.empID ==parseInt(id));
+    // console.log(id);
+    if(!details){
+        return res.status(404).json({error:'task not found'});
+    }
+    // console.log(details)
+    res.sendFile(path.join(__dirname,'public','search.html'))
+
+})
+
+
+app.get('/search/data/:id', (req, res) => {
+    const id = req.params.id;
+   
+    const details = employeeDetails.find(emp => emp.empID === parseInt(id)); // Ensure the types match
+    console.log("search details:")
+    console.log(details);
+
+    res.json(details);
+});
+
+
+app.post('/delete/:id', (req, res) => {
+    const id = req.params.id;
+    console.log('Before deletion:', employeeDetails);
+    
+    employeeDetails = employeeDetails.filter(emp => emp.empID !==parseInt(id));
+
+    console.log('After deletion:', employeeDetails);
+    res.redirect('/Details');
+});
 
 
 
